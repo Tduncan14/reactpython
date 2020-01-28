ca25c3c39cc0b679eef0e87083313d6f589ef0e7
Treek <nbatre1@yahoo.com>
2020-01-28 13:48:52 -0600

    keep moving forward

--- python-graphene/schema.py
+++ python-graphene/schema.py
@@ -1,11 +1,36 @@
 import graphene
+import json
+from datetime import datetime
+
+
+class User(graphene.ObjectType):
+    id = graphene.ID()
+    username = graphene.String()
+    created_at = graphene.DateTime()
+
+
+
+
+
 
 class Query(graphene.ObjectType):
+    users = graphene.List(User)
     hello = graphene.String()
+    is_admin = graphene.Boolean()
 
 
     def resolve_hello(self,info):
         return "world"
+
+    def resolve_is_admin(self,info):
+        return True
+
+    def resolve_users(self,info):
+        return [
+          User(id="1",username="Fred",created_at = datetime.now()),
+           User(id="2",username="Tred",created_at = datetime.now()),
+            User(id="3",username="Lred",created_at = datetime.now())
+        ]
 
 
 
@@ -14,14 +39,18 @@ schema = graphene.Schema(query = Query)
 result = schema.execute(
     '''
    {
-    hello
+     users{
+         id
+         username
+         createdAt
+     }
 
    }
    '''
 )
 
 
-print(result.data.items())
+dictResult = dict(result.data.items())
 
 
-
+print(json.dumps(dictResult, indent =2))
